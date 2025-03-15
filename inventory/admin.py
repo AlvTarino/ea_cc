d53c5cd25f96ae2cecc66c3bed01e5d0b126fb50
AlvTarino <Alvtarino@outlook.com>
2025-03-15 14:30:04 +0300

    inventory admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,13 @@
+# inventory/admin.py
 from django.contrib import admin
+from .models import StockAdjustment
 
-# Register your models here.
+
+@admin.register(StockAdjustment)
+class StockAdjustmentAdmin(admin.ModelAdmin):
+    list_display = ('product', 'adjustment', 'reason', 'date')
+    list_filter = ('product', 'date')
+    search_fields = ('product', 'reason')
+    date_hierarchy = 'date'
+    ordering = ('-date',)
+    actions = ['export_as_csv']
